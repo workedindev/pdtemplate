f5caf5d6ebcb9c9fb0ce4c8c85483d177e920a84
workedindev <github@workedindev.com>
2023-12-09 20:57:09 -0600

    initial push

---
+++ source/main.lua
@@ -0,0 +1,16 @@
+import "CoreLibs/graphics"
+
+local pd <const> = playdate
+local gfx <const> = pd.graphics
+
+local playerX, playerY = 200, 120
+local playerRadius = 10
+local playerSpeed = 3
+
+function playdate.update()
+	gfx.clear()
+	local crankAngle = math.rad(pd.getCrankPosition())
+	playerX = playerX + math.sin(crankAngle) * playerSpeed
+	playerY = playerY + math.cos(crankAngle) * playerSpeed
+	gfx.fillCircleAtPoint(playerX, playerY, playerRadius)
+end
